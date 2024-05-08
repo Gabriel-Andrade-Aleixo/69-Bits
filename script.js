b5d335ba4eb1ad622de67f02063fe2d5b6936f7e
Gabriel-Andrade-Aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-05-07 21:58:49 -0300

    rolagem ao atualizar

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('scrollh', window.scrollY > 100)
 })
+
+window.onload = function() {
+    window.scrollTo(0, 0);
+}
