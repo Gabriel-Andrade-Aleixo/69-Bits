236dab8336f19137cbe0bbcbb5c46023e09b6b07
Gabriel-Andrade-Aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-05-08 21:56:52 -0300

    comentarios

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     let header = document.querySelector('#header')
     header.classList.toggle('scrollh', window.scrollY > 100)
 })
 
-window.onload = function() {
+window.onload = function () {
     window.scrollTo(0, 0);
+}
+
+function enviarFormulario() {
+    var formulario = document.getElementById('meuFormulario');
+    var campos = formulario.getElementsByTagName('input');
+    for (var i = 0; i < campos.length; i++) {
+        campos[i].value = '';
+    }
+
+    var mensagem = document.getElementById('mensagem');
+    mensagem.style.display = 'flex';
+    var mensagem = document.getElementById('mensagem');
+    mensagem.style.justifyContent = 'center';
 }
